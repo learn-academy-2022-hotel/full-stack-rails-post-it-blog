e3e45f5b741a32d497cb8ca62b08917a6a137dc1
priceymineral <tudaeva@gmail.com>
2023-01-25 17:25:50 -0800

    added specs and validations for the presence of title and content when creating a blog

--- spec/models/blog_spec.rb
+++ spec/models/blog_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Blog, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # Test confirms the prescence of title in a blog
+  it "confirms the prescence of a title in the blog" do 
+    # check that the errors object for the title attribute is not empty
+    blog = Blog.create(content:"Austin just got a job! Congrats!") 
+    expect(blog.errors[:title]).to_not be_empty
+  end
+
+  # Test confirms the presence of content in a blog
+  it "confirms the prescence of a content in the blog" do 
+    # check that the errors object for the content attribute is not empty
+    blog = Blog.create(title:"Get a job!") 
+    expect(blog.errors[:content]).to_not be_empty
+  end
 end
